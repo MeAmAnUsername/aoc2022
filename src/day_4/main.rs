e19a44cd0421a47e18ddb90eeb481530eb7c41b9
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2022-12-04 06:29:54 +0100

    Day 4: Camp Cleanup - part 2

--- src/day_4/main.rs
+++ src/day_4/main.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Solution: {}", solve_part1(INPUT));
+    println!("Solution: {}", solve_part2(INPUT));
 }
 
 // answer part 1: 644
@@ -37,4 +37,36 @@ fn example_part1() {
 2-6,4-8";
 
     assert_eq!(2, solve_part1(INPUT))
+}
+
+// answer part 2: 926
+fn solve_part2(input: &str) -> usize {
+    let regex: Regex = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
+    input
+        .lines()
+        .filter(|pair| {
+            dbg!(pair);
+            let mtch = regex
+                .captures(pair)
+                .expect(format!("pair does not match regex: {}", pair).as_str());
+            let left_start: u32 = mtch.get(1).unwrap().as_str().parse::<u32>().unwrap();
+            let left_end: u32 = mtch.get(2).unwrap().as_str().parse::<u32>().unwrap();
+            let right_start: u32 = mtch.get(3).unwrap().as_str().parse::<u32>().unwrap();
+            let right_end: u32 = mtch.get(4).unwrap().as_str().parse::<u32>().unwrap();
+            (left_end >= right_start && left_start <= right_end)
+                || (right_end >= left_start && right_start <= left_end)
+        })
+        .count()
+}
+
+#[test]
+fn example_part2() {
+    const INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+    assert_eq!(4, solve_part2(INPUT))
 }
