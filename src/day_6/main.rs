a126d0eaa21a85c49736a2ec4cee9d87e78170d3
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2022-12-29 14:10:06 +0100

    Day 6: Tuning Trouble - part 1

---
+++ src/day_6/main.rs
@@ -0,0 +1,62 @@
+#![allow(dead_code)]
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    println!("Solution: {}", solve_part1(INPUT));
+}
+
+// answer part 1: 1833
+pub fn solve_part1(input: &str) -> usize {
+    input
+        .chars()
+        .tuple_windows::<(_, _, _, _)>()
+        .enumerate()
+        .filter(|(_, (a, b, c, d))| [a, b, c, d].iter().all_unique())
+        .nth(0)
+        .expect("No window with only unique elements found")
+        .0
+        + 3
+        + 1 // first window already contains 3 characters, +1 for 1 based counting
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1_part1() {
+        const INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+        assert_eq!(7, solve_part1(INPUT))
+    }
+
+    #[test]
+    fn example2_part1() {
+        const INPUT: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+
+        assert_eq!(5, solve_part1(INPUT))
+    }
+
+    #[test]
+    fn example3_part1() {
+        const INPUT: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+
+        assert_eq!(6, solve_part1(INPUT))
+    }
+
+    #[test]
+    fn example4_part1() {
+        const INPUT: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+
+        assert_eq!(10, solve_part1(INPUT))
+    }
+
+    #[test]
+    fn example5_part1() {
+        const INPUT: &str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+        assert_eq!(11, solve_part1(INPUT))
+    }
+}
